62a2deff1cce1aee16628e07191495187fba1d68
chrisdh09 <chris09.delgado@gmail.com>
2026-03-24 22:39:39 -0600

    project01

--- I_Cuatri/proyecto/JS/index.js
+++ I_Cuatri/proyecto/JS/index.js
@@ -10,34 +10,52 @@ El precio de los tiquetes sigues las siguientes reglas:
     con id de estudiante y con un cupón de 30% de descuento, solo aplicará uno de los dos descuentos.
 */
 
-const age = 63;
+const age = 65;
 const hasCoupon = true;
-const couponDiscount = 10;
-const hasStudentId = true;
-
+const couponDiscount = 99;
+const hasStudentId = false;
 let basePrice = 5000
 let finalPrice = 0
 
 // Escriba su código aquí calculando el price
 
-//1.Niños menores de 12 años entran gratis
-if (age<12) {
-    finalPrice=0;}
-
-    // Personas mayores de 65 años tienen un 40% de descuento
-    else if (age>= 65){
-        finalPrice = basePrice - (basePrice*0.40)}
-        // Personas con un id de estudiantes tienen un 25% de descuento, sin importar la edad
-        else if (hasStudentId==true){
-            finalPrice = basePrice - (basePrice*0.25)}
-             //Personas con cupón de descuento, se les puede aplicar el descuento descrito en el cupón.
-            else if (couponDiscount==10){
+//Niños menores de 12 años entran gratis
+if (age < 12) {
+    finalPrice = 0;
 } else {
+
+    //descuentos
+    let discountAdult = 0;
+    let discountStudent = 0;
+    let discountCoupon = 0;
+
+    //persona mayor de edad
+    if (age >= 65) {
+        discountAdult = 40;
+    }
+
+    //persona estudiante
+    if (hasStudentId) {  // aqui no pongo ""== true" por que siempre va a verificar si esta true aunque no le especifique
+        discountStudent = 25;
+    }
+
+    // Cupon cualquiera
+    if (hasCoupon) {
+        discountCoupon = couponDiscount;
+    }
+
+    //Aqui se elige el descuento mayor de los antes declarados para aplicar solo 1
+    let bestDiscount = Math.max(discountAdult, discountStudent, discountCoupon); // bestDiscount pasa a ser el mejor descuento
+    let finalDiscount = basePrice * (bestDiscount / 100);
+
     
+
+    finalPrice = basePrice - finalDiscount;
+
 }
 
 // Resultado final con el price calculado:
-console.log('El precio de su tiquete es: ', finalPrice)
+    console.log("El precio de su tiquete es: ", finalPrice) // este tiene que ir fuera del if para que se ejecute tanto si se cumple el IF o el ELSE
 
 // Ejemplos:
 
