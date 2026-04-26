af8a34e7de340989ea92c9920204842f0131096f
chrisdh09 <chris09.delgado@gmail.com>
2026-04-26 00:44:14 -0600

    clase 12 y 13

---
+++ I_Cuatri/Clase_12/main.js
@@ -0,0 +1,16 @@
+const list = [32, 87, 999, 1777232, 4, 1, 54, 111];
+const pares = [];
+const impares = [];
+
+for (let i = 0; i < array.length; i++) {
+    if (list[i] % 2 === 0) {
+        pares.push(list[i]);
+    } else {
+        odd.push(list[i]);
+    }
+    
+}
+
+console.log("Pares : ", pares);
+console.log("Impares: ", impares);
+
---
+++ I_Cuatri/Clase_13/main.js
@@ -0,0 +1,28 @@
+//VARIABLES
+const variable = 23;
+variable = 29; //esto da error al tratar de cambiar el const
+let variable2= 22;
+variable2 = 49 // aqui si no da error por que es un varibale let y esta si se puede cambiar
+var variableVar = 78 // casi no se usa
+
+//Tipo de datos undefined, null
+const variableNull = null;
+const variableNumber = 34.23;
+const variableString = "hola";
+const buleano = true;
+//truthy
+//falsy
+
+
+//Operadores
+let notVariable = !(7===7); // TRUE negation
+let doubleNot = !!(7===7) // TRUE double negation
+//>,<,<=,>=
+//&&
+const trueAndTrue = true && true;
+const  trueanFalse = t
+
+//Condicionales
+
+//Bucles
+
